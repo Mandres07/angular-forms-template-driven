55e1ba16e446445916ad8f9959147f32ea0bf8f2
Mario Morales <mario.morales@devsu.com>
2021-10-04 10:47:56 -0500

    added forms validation with template driven

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
    selector: 'app-root',
@@ -6,7 +7,30 @@ import { Component } from '@angular/core';
    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+   defaultQuestion = 'pet';
+   answer = '';
+   genders = ['male', 'female'];
+   name = '';
+   user = {
+      username: '',
+      email: '',
+      secretQuestion: '',
+      secretAnswer: '',
+      gender: ''
+   };
+   submitted = false;
+
    suggestUserName() {
       const suggestedName = 'Superuser';
+      this.name = suggestedName;
+   }
+
+   onSubmit(form: NgForm) {
+      this.user.username = form.value.userData.username;
+      this.user.email = form.value.userData.email;
+      this.user.secretQuestion = form.value.secret;
+      this.user.secretAnswer = form.value.questionAnswer;
+      this.user.gender = form.value.gender;
+      this.submitted = true;
    }
 }
